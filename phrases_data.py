3137c1ecd0920330b1a74289ff59978470364d58
Dezzy <134234868+DezSquid@users.noreply.github.com>
2024-04-17 16:12:56 -0400

    Create phrases_data.py

---
+++ phrases_data.py
@@ -0,0 +1,46 @@
+phrases = {
+    'Common': [
+        ('Beta', 5),
+        ('Gen Alpha', 10),
+        ('Griddy', 20)
+    ],
+    'Uncommon': [
+        ('Sprite Cranberry', 25),
+        ('Blue Smurf Cat', 35),
+        ('Big Chungus', 50),
+        ('Super Idol', 75)
+    ],
+    'Rare': [
+        ('Lebron Sunshine', 100),
+        ('Hood Irony', 150),
+        ('Airpod Shotty', 200),
+        ('Livvy Dunne', 250)
+    ],
+    'Ultra Rare': [
+        ('Grimace Shake', 1000),
+        ('Adin Ross', 2500),
+        ('Mogwarts', 5000),
+        ('IShowSpeed', 10000)
+    ],
+    'Legendary': [
+        ('TikTok Rizz party', 10000),
+        ('Giga Chad', 25000),
+        ('Mewing', 50000),
+        ('Rizz', 100000)
+    ],
+    'Exotic': [
+        ('Ohio', 250000),
+        ('Fanum Tax', 500000),
+        ('Among Us', 1000000),
+        ('Ice Spice', 1500000)
+    ],
+    'Mythical': [
+        ('Sigma', 2000000),
+        ('Quandale Dingle', 5000000),
+        ('Duke Dennis', 10000000),
+        ('Kai Cenat', 20000000)
+    ],
+    'Interstellar': [
+        ('Skibidi Toilet', 50000000)
+    ]
+}
